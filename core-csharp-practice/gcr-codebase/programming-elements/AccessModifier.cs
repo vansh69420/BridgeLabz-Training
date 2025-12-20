2512713f98f6f3d7dfd6f4a6c4f307f08bdd38cd
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-12-20 10:56:15 +0530

    [Vansh Tyagi].Added AccessModifier.cs File in Programming Elements

--- core-csharp-practice/gcr-codebase/programming-elements/AccessModifier.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/AccessModifier.cs
@@ -6,6 +6,16 @@ class AccessModifier
 		PublicClass pub = new PublicClass();
         pub.PublicMethod();
         Console.WriteLine(pub.publicVar);
+
+        InternalClass inter = new InternalClass();
+        Console.WriteLine(inter.internalVar);
+
+        BaseClass baseObj = new BaseClass();
+        baseObj.ShowBaseData();
+
+        DerivedClass derived = new DerivedClass();
+        derived.ShowDerivedData();
+
 	}
 	public class PublicClass
 	{
@@ -16,4 +26,45 @@ class AccessModifier
 			Console.WriteLine("Public Method");
 		}
 	}
+
+	internal class InternalClass
+	{
+    	internal int internalVar = 20;
+	}
+
+	class BaseClass
+	{
+	    public int publicData = 1;
+	    private int privateData = 2;
+	    protected int protectedData = 3;
+	    internal int internalData = 4;
+	    protected internal int protectedInternalData = 5;
+	    private protected int privateProtectedData = 6;
+
+	    public void ShowBaseData()
+	    {
+	        Console.WriteLine("Inside BaseClass:");
+	        Console.WriteLine(publicData);
+	        Console.WriteLine(privateData);
+	        Console.WriteLine(protectedData);
+	        Console.WriteLine(internalData);
+	        Console.WriteLine(protectedInternalData);
+	        Console.WriteLine(privateProtectedData);
+	    }
+	}
+
+	class DerivedClass : BaseClass
+	{
+	    public void ShowDerivedData()
+	    {
+	        Console.WriteLine("\nInside DerivedClass:");
+	        Console.WriteLine(publicData);              
+	        // Console.WriteLine(privateData);
+	        Console.WriteLine(protectedData);           
+	        Console.WriteLine(internalData);            
+	        Console.WriteLine(protectedInternalData);   
+	        Console.WriteLine(privateProtectedData);    
+	    }
+	}
+
 }
