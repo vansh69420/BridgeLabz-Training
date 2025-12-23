00fca141afa3242e6155a59fd4cbc8c0ebda493c
Vansh Tyagi <vanshtyagi2017.vt@gmail.com>
2025-12-23 21:22:48 +0530

    [Vansh Tyagi].Added 2 Problems in Leet Code Codebase Folder

---
+++ core-csharp-practice/leet-code-based/RemoveElement.cs
@@ -0,0 +1,19 @@
+using System;
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int idx = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        return idx;
+    }
+}
